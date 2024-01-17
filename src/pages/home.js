76c91728a4cea64e3cc279086a96edca1eed132a
BCR-0632 <caiolopesfv@gmail.com>
2024-01-16 21:08:45 -0300

    enviando atualizações

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,19 @@
 import './home.css'; 
 
+import { Link } from 'react-router-dom';
+
 function Home () {
 
     return <div className="container">
 
         <section className='presentation'>
+            <h2>
+                Experiência para <br />o resto da <span>vida.</span>   
+            </h2>
 
+            <p>———— Desde 2022 ————</p>
+
+            <a >Agendar seção</a>
         </section>
 
     </div>

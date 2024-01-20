429fb85cce37e5a7d98bb4fe9312afbe13a7bf99
BCR-0632 <caiolopesfv@gmail.com>
2024-01-20 18:18:52 -0300

    enviando atualizações

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,7 +13,67 @@ function Home () {
 
             <p>———— Desde 2022 ————</p>
 
-            <a >Agendar seção</a>
+            <a 
+            href='https://wa.me/5511968705571'
+            target='_blank'>Agendar seção
+            </a>
+        </section>
+
+        <section className='video-container'>
+
+            <div className='video-content'>
+                <h2>Atendendo <span>as suas <br /> expectativas</span></h2>
+                <div className='line'></div>
+                <p>
+                    Algum texto comercial que tenha como intenção
+                    atrair clientes ou criar uma espécie de convite
+                    para tatuar e tudo mais. <br />
+                    Algum texto comercial que tenha como intenção
+                    atrair clientes ou criar uma espécie de convite
+                    para tatuar e tudo mais. 
+                </p>
+            </div>
+
+            <div className='video-presentation'>
+                <button onClick={() => {
+                    alert('video btn clicado'); 
+                }}>
+                    <img src='./assets/play-icon.png' />
+                </button>
+            </div>
+
+        </section>
+
+        <section className='services-container'>
+            <div className='service-cards'>
+                <div className='card'><p><Link>REALISMO</Link></p></div>
+
+                <div className='card'><p><Link>BLACK WORK</Link></p></div>
+
+                <div className='card'><p><Link>FINE LINE</Link></p></div>
+
+                <div className='card'><p><Link>COBERTURA</Link></p></div>
+            </div>  
+
+            <div className='service-content'>
+
+                <h2>Estilos de <span>Tattoo</span></h2>
+                <div className='line'></div>
+                <p>
+                    Algum texto comercial que tenha como intenção
+                    atrair clientes ou criar uma espécie de convite
+                    para tatuar e tudo mais. <br />
+                    Algum texto comercial que tenha como intenção
+                    atrair clientes ou criar uma espécie de convite
+                    para tatuar e tudo mais. 
+                </p>
+                <a 
+                href='https://wa.me/5511968705571'
+                target='_blank'>Agendar seção
+                </a>
+
+            </div>
+
         </section>
 
     </div>
